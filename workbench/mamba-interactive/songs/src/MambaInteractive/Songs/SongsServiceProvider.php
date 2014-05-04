cb090f13a832bef5b61525b2a8e27f713b85a672
momba <iammomba@gmail.com>
2014-05-04 11:20:04 -0400

    Initial
    Initial Commit

---
+++ workbench/mamba-interactive/songs/src/MambaInteractive/Songs/SongsServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace MambaInteractive\Songs;
+
+use Illuminate\Support\ServiceProvider;
+
+class SongsServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mamba-interactive/songs');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
